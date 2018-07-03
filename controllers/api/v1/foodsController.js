b2f3233dc86eeb6b8e2f12bbfdc1eb30b09a1bae
Evan Wheeler <anon0mys87@gmail.com>
2018-07-02 21:19:47 -0600

    Add sad path testing for food not found on update

--- controllers/api/v1/foodsController.js
+++ controllers/api/v1/foodsController.js
@@ -33,7 +33,12 @@ const show = (req, res, next) => {
 const update = (req, res, next) => {
   Food.update(req.params.id, req.body)
     .then((food) => {
-      res.json(food[0])
+      if(food.length > 0) {
+        res.json(food[0])
+      }
+      else {
+        res.status(404).send()
+      }
     })
     .catch((err) => {
       res.status(400).send()
--- test/endpoints/api/v1/foods/update_food.test.js
+++ test/endpoints/api/v1/foods/update_food.test.js
@@ -30,4 +30,19 @@ describe('PUTS /api/v1/foods', () => {
           })
       })
   })
+
+  it('returns 404 if food not found', (done) => {
+    const food = database('foods')
+      .insert({ 'name': 'Pizza', 'calories': '100'})
+      .returning(['id', 'name', 'calories'])
+      .then((food) => {
+        chai.request(app)
+          .put('/api/v1/foods/200')
+          .send({ 'name': 'Changed'})
+          .end((err, res) => {
+            res.should.have.status(404)
+            done()
+          })
+      })
+  })
 })
