38663c757da418dc5f7a3b3d79976a878f045f55
Evan Wheeler <anon0mys87@gmail.com>
2018-07-03 08:44:39 -0600

    Bug fixes and cors issues

--- db/migrations/20180703004802_create-meal-foods-table.js
+++ db/migrations/20180703004802_create-meal-foods-table.js
@@ -1,9 +1,9 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('meal_foods', function(t) {
-    t.increments().primary()
-    t.integer('food_id').unsigned().references('id').inTable('foods')
-    t.integer('meal_id').unsigned().references('id').inTable('meals')
+    t.increments('id').primary()
+    t.integer('food_id').references('id').inTable('foods')
+    t.integer('meal_id').references('id').inTable('meals')
   })
 };
 
